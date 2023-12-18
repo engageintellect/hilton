8e1bdfe58ef0dd0a3a24e27b88f37ee58cefe9f6
engageintellect <jc9361@gmail.com>
2023-12-18 00:39:57 -1000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,84 @@
+<div>
+	<div class="relative bg-gradient-to-b from-blue-500 to-purple-500">
+		<img
+			class="h-screen w-full object-cover opacity-30"
+			src="https://images.unsplash.com/photo-1591307806437-cc4da031689b?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt=""
+		/>
+		<div
+			class="absolute top-0 flex h-full w-full items-center justify-center font-bold text-white drop-shadow-lg"
+		>
+			<div class="flex flex-col items-center justify-center">
+				<div class="text-7xl">Welcome to our village.</div>
+				<div class="my-5 flex gap-5">
+					<button class="rounded border border-white px-4 py-2 uppercase drop-shadow-lg"
+						>click me</button
+					>
+					<button>click me</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="h-screen">
+		<div
+			class="grid h-full w-full grid-cols-1 bg-gradient-to-b from-purple-500 to-orange-500 md:grid-cols-2"
+		>
+			<div class="relative h-full w-full">
+				<img
+					class="absolute inset-0 h-full w-full object-cover opacity-50"
+					src="https://images.unsplash.com/photo-1596692434692-56e12c64f147?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<div
+					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					style="opacity: 1;"
+				>
+					Stuff to do
+				</div>
+			</div>
+
+			<div class="relative h-full w-full">
+				<img
+					class="absolute inset-0 h-full w-full object-cover opacity-50"
+					src="https://images.unsplash.com/photo-1596404575621-5887511bf44c?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<div
+					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					style="opacity: 1;"
+				>
+					Food
+				</div>
+			</div>
+
+			<div class="relative h-full w-full">
+				<img
+					class="absolute inset-0 h-full w-full object-cover opacity-50"
+					src="https://images.unsplash.com/photo-1591307806437-cc4da031689b?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<div
+					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					style="opacity: 1;"
+				>
+					Events
+				</div>
+			</div>
+
+			<div class="relative h-full w-full">
+				<img
+					class="absolute inset-0 h-full w-full object-cover opacity-50"
+					src="https://images.unsplash.com/photo-1591307806437-cc4da031689b?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<div
+					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					style="opacity: 1;"
+				>
+					Map
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
