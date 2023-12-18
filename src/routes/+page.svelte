b07ee1d367b2b2e6b2d5dbef36701e9a04f8e056
engageintellect <jc9361@gmail.com>
2023-12-18 00:50:20 -1000

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,14 @@
 			alt=""
 		/>
 		<div
-			class="absolute top-0 flex h-full w-full items-center justify-center font-bold text-white drop-shadow-lg"
+			class="absolute top-0 flex h-full w-full items-center justify-center text-white drop-shadow-lg"
 		>
-			<div class="flex flex-col items-center justify-center">
-				<div class="text-7xl">Welcome to our village.</div>
+			<div class="flex flex-col items-center justify-center px-6">
+				<div class="text-7xl font-bold">Welcome To Our Village.</div>
+				<div class="mt-5 w-full max-w-2xl text-xl">
+					Lorem cillum pariatur nostrud veniam ipsum aliqua ullamco velit ad qui consequat. Proident
+					culpa culpa proident occaecat non esse eu nostrud enim sit. In sit ut do deserunt. Ex non
+				</div>
 				<div class="my-5 flex gap-5">
 					<button class="rounded border border-white px-4 py-2 uppercase drop-shadow-lg"
 						>click me</button
@@ -26,12 +30,12 @@
 		>
 			<div class="relative h-full w-full">
 				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-50"
-					src="https://images.unsplash.com/photo-1596692434692-56e12c64f147?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					class="absolute inset-0 h-full w-full object-cover opacity-30"
+					src="https://www.surfertoday.com/images/stories/luau.jpg"
 					alt=""
 				/>
 				<div
-					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
 					style="opacity: 1;"
 				>
 					Stuff to do
@@ -40,12 +44,12 @@
 
 			<div class="relative h-full w-full">
 				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-50"
-					src="https://images.unsplash.com/photo-1596404575621-5887511bf44c?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					class="absolute inset-0 h-full w-full object-cover opacity-30"
+					src="https://res.klook.com/image/upload/q_85/c_fill,w_750/v1662966121/blog/bnc8qabcisjdoa2zvnko.jpg"
 					alt=""
 				/>
 				<div
-					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
 					style="opacity: 1;"
 				>
 					Food
@@ -54,12 +58,12 @@
 
 			<div class="relative h-full w-full">
 				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-50"
-					src="https://images.unsplash.com/photo-1591307806437-cc4da031689b?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					class="absolute inset-0 h-full w-full object-cover opacity-30"
+					src="https://cdn2.veltra.com/ptr/20221201023017_634450703_10532_0.jpg?imwidth=550&impolicy=custom"
 					alt=""
 				/>
 				<div
-					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
 					style="opacity: 1;"
 				>
 					Events
@@ -68,12 +72,12 @@
 
 			<div class="relative h-full w-full">
 				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-50"
-					src="https://images.unsplash.com/photo-1591307806437-cc4da031689b?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					class="absolute inset-0 h-full w-full object-cover opacity-30"
+					src="https://www.flyinghighonpoints.com/wp-content/uploads/2018/07/HHVmap-1200x919.jpg"
 					alt=""
 				/>
 				<div
-					class="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
 					style="opacity: 1;"
 				>
 					Map
