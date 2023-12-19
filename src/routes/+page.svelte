9f0d6a934002e82c4df6587b5a4d4d46d19bff04
engageintellect <jc9361@gmail.com>
2023-12-18 16:14:45 -1000

    init2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,80 +9,99 @@
 			class="absolute top-0 flex h-full w-full items-center justify-center text-white drop-shadow-lg"
 		>
 			<div class="flex flex-col items-center justify-center px-6">
-				<div class="text-7xl font-bold">Welcome To Our Village.</div>
-				<div class="mt-5 w-full max-w-2xl text-xl">
+				<div class="text-5xl font-bold sm:text-7xl">Welcome To Our Village.</div>
+				<div class="mt-5 w-full max-w-2xl font-thin sm:text-xl">
 					Lorem cillum pariatur nostrud veniam ipsum aliqua ullamco velit ad qui consequat. Proident
 					culpa culpa proident occaecat non esse eu nostrud enim sit. In sit ut do deserunt. Ex non
 				</div>
-				<div class="my-5 flex gap-5">
-					<button class="rounded border border-white px-4 py-2 uppercase drop-shadow-lg"
-						>click me</button
-					>
-					<button>click me</button>
+
+				<div
+					class="my-5 flex w-full flex-col justify-center gap-2 uppercase sm:max-w-lg sm:flex-row"
+				>
+					<div class="w-full">
+						<a href="/" class="flex w-full justify-center rounded bg-blue-500 px-6 py-4 shadow"
+							>Contact Us</a
+						>
+					</div>
+					<div class="w-full">
+						<a
+							href="/#menu"
+							class="flex w-full justify-center rounded bg-indigo-500 px-6 py-4 shadow"
+							>Get Started</a
+						>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<div class="h-screen">
+	<div class="h-screen" id="menu">
 		<div
 			class="grid h-full w-full grid-cols-1 bg-gradient-to-b from-purple-500 to-orange-500 md:grid-cols-2"
 		>
-			<div class="relative h-full w-full">
-				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-30"
-					src="https://www.surfertoday.com/images/stories/luau.jpg"
-					alt=""
-				/>
-				<div
-					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
-					style="opacity: 1;"
-				>
-					Stuff to do
+			<a href="/events">
+				<div class="group relative h-full w-full cursor-pointer">
+					<img
+						class="absolute inset-0 h-full w-full object-cover opacity-30 transition-opacity duration-200 md:group-hover:opacity-50"
+						src="https://www.surfertoday.com/images/stories/luau.jpg"
+						alt=""
+					/>
+					<div
+						class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
+						style="opacity: 1;"
+					>
+						Stuff to do
+					</div>
 				</div>
-			</div>
+			</a>
 
-			<div class="relative h-full w-full">
-				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-30"
-					src="https://res.klook.com/image/upload/q_85/c_fill,w_750/v1662966121/blog/bnc8qabcisjdoa2zvnko.jpg"
-					alt=""
-				/>
-				<div
-					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
-					style="opacity: 1;"
-				>
-					Food
+			<a href="/food">
+				<div class="group relative h-full w-full cursor-pointer">
+					<img
+						class="absolute inset-0 h-full w-full object-cover opacity-30 transition-opacity duration-200 md:group-hover:opacity-50"
+						src="https://res.klook.com/image/upload/q_85/c_fill,w_750/v1662966121/blog/bnc8qabcisjdoa2zvnko.jpg"
+						alt=""
+					/>
+					<div
+						class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
+						style="opacity: 1;"
+					>
+						Food
+					</div>
 				</div>
-			</div>
+			</a>
 
-			<div class="relative h-full w-full">
-				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-30"
-					src="https://cdn2.veltra.com/ptr/20221201023017_634450703_10532_0.jpg?imwidth=550&impolicy=custom"
-					alt=""
-				/>
-				<div
-					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
-					style="opacity: 1;"
-				>
-					Events
+			<a href="/events">
+				<div class="group relative h-full w-full cursor-pointer">
+					<img
+						class="absolute inset-0 h-full w-full object-cover opacity-30 transition-opacity duration-200 md:group-hover:opacity-50"
+						src="https://cdn2.veltra.com/ptr/20221201023017_634450703_10532_0.jpg?imwidth=550&impolicy=custom"
+						alt=""
+					/>
+					<div
+						class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
+						style="opacity: 1;"
+					>
+						Events
+					</div>
 				</div>
-			</div>
+			</a>
 
-			<div class="relative h-full w-full">
-				<img
-					class="absolute inset-0 h-full w-full object-cover opacity-30"
-					src="https://www.flyinghighonpoints.com/wp-content/uploads/2018/07/HHVmap-1200x919.jpg"
-					alt=""
-				/>
-				<div
-					class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
-					style="opacity: 1;"
-				>
-					Map
+			<a href="/map">
+				<div class="group relative h-full w-full cursor-pointer">
+					<img
+						class="absolute inset-0 h-full w-full object-cover opacity-30 transition-opacity duration-200 md:group-hover:opacity-50"
+						src="https://www.flyinghighonpoints.com/wp-content/uploads/2018/07/HHVmap-1200x919.jpg"
+						alt=""
+					/>
+					<div
+						class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white drop-shadow"
+						style="opacity: 1;"
+					>
+						Map
+					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 	</div>
 </div>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,5 @@
+<img
+	class="h-full w-full"
+	src="https://www.flyinghighonpoints.com/wp-content/uploads/2018/07/HHVmap-1200x919.jpg"
+	alt=""
+/>
