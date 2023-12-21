93291e6c3cde80bd9575720eaaa7d9d2f5a80768
engageintellect <jc9361@gmail.com>
2023-12-21 02:25:17 -1000

    init4

--- src/lib/components/CardRow.svelte
+++ src/lib/components/CardRow.svelte
@@ -5,34 +5,78 @@
 	export let photo = 'Photo';
 </script>
 
-<div class="mx-auto h-full w-full max-w-5xl overflow-x-scroll p-2">
+<div class="mx-auto h-full w-full max-w-5xl overflow-x-scroll">
 	<div class="flex flex-row gap-2">
-		<div class="h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
-			<div>{name}</div>
-			<div>{description}</div>
-			<div>{type}</div>
-			<div>{photo}</div>
+		<div class="relative h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
+			<div class="h-full w-full overflow-hidden rounded">
+				<img
+					src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+					alt={name}
+					class="h-full w-full object-cover"
+				/>
+			</div>
+
+			<div
+				class="absolute inset-0 flex flex-col items-center justify-center text-center text-white opacity-0 transition-opacity duration-300 hover:opacity-100"
+			>
+				<div>{name}</div>
+				<div>{description}</div>
+				<div>{type}</div>
+			</div>
 		</div>
 
-		<div class="h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
-			<div>{name}</div>
-			<div>{description}</div>
-			<div>{type}</div>
-			<div>{photo}</div>
+		<div class="relative h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
+			<div class="h-full w-full overflow-hidden rounded">
+				<img
+					src="https://t4.ftcdn.net/jpg/02/86/17/89/360_F_286178925_8zk89O9uC5JJVPvqhvBMUpaRxp8AFXzD.jpg"
+					alt={name}
+					class="h-full w-full object-cover"
+				/>
+			</div>
+
+			<div
+				class="absolute inset-0 flex flex-col items-center justify-center text-center text-white opacity-0 transition-opacity duration-300 hover:opacity-100"
+			>
+				<div>{name}</div>
+				<div>{description}</div>
+				<div>{type}</div>
+			</div>
 		</div>
 
-		<div class="h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
-			<div>{name}</div>
-			<div>{description}</div>
-			<div>{type}</div>
-			<div>{photo}</div>
+		<div class="relative h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
+			<div class="h-full w-full overflow-hidden rounded">
+				<img
+					src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"
+					alt={name}
+					class="h-full w-full object-cover"
+				/>
+			</div>
+
+			<div
+				class="absolute inset-0 flex flex-col items-center justify-center text-center text-white opacity-0 transition-opacity duration-300 hover:opacity-100"
+			>
+				<div>{name}</div>
+				<div>{description}</div>
+				<div>{type}</div>
+			</div>
 		</div>
 
-		<div class="h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
-			<div>{name}</div>
-			<div>{description}</div>
-			<div>{type}</div>
-			<div>{photo}</div>
+		<div class="relative h-64 w-2/3 flex-shrink-0 rounded border p-2 shadow sm:w-1/3">
+			<div class="h-full w-full overflow-hidden rounded">
+				<img
+					src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+					alt={name}
+					class="h-full w-full object-cover"
+				/>
+			</div>
+
+			<div
+				class="absolute inset-0 flex flex-col items-center justify-center text-center text-white opacity-0 transition-opacity duration-300 hover:opacity-100"
+			>
+				<div>{name}</div>
+				<div>{description}</div>
+				<div>{type}</div>
+			</div>
 		</div>
 	</div>
 </div>
--- src/routes/food/+page.svelte
+++ src/routes/food/+page.svelte
@@ -2,32 +2,31 @@
 	import CardRow from '$lib/components/CardRow.svelte';
 </script>
 
-<div class="mx-auto my-10 w-full max-w-5xl">
+<div class="mx-auto my-10 w-full max-w-5xl p-2">
 	<div class="text-center text-5xl font-bold">Food</div>
 
-	<div class="my-5">
-		<div class="text-lg font-bold uppercase">Resturants</div>
+	<div class="my-5" id="resturaunts">
+		<div class="text-lg font-bold uppercase">
+			<a href="#resturaunts">Resturaunts</a>
+		</div>
 		<div>
 			<CardRow />
 		</div>
 	</div>
 
-	<div class="my-5">
-		<div class="text-lg font-bold uppercase">Snacks</div>
+	<div class="my-5" id="snacks">
+		<div class="text-lg font-bold uppercase">
+			<a href="#snacks">Snacks</a>
+		</div>
 		<div>
 			<CardRow />
 		</div>
 	</div>
 
-	<div class="my-5">
-		<div class="text-lg font-bold uppercase">Desserts</div>
-		<div>
-			<CardRow />
+	<div class="my-5" id="groceries">
+		<div class="text-lg font-bold uppercase">
+			<a href="#groceries">Groceries</a>
 		</div>
-	</div>
-
-	<div class="my-5">
-		<div class="text-lg font-bold uppercase">Groceries</div>
 		<div>
 			<CardRow />
 		</div>
